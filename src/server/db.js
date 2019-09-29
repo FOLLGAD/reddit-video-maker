8417a80306eb8db5b7c5d6d8ad338f428faf14ed
Emil Ahlbäck <emil.ahlback99@gmail.com>
2019-09-29 21:41:43 +0200

    Made DB_URL an env variable

--- src/server/db.js
+++ src/server/db.js
@@ -1,10 +1,12 @@
 const mongoose = require('mongoose')
 
+require('dotenv').config()
+
 module.exports.connect = () => {
 	mongoose.set('useCreateIndex', true)
 
 	return mongoose
-		.connect('mongodb://localhost/project_bog', {
+		.connect(process.env.DB_URL, {
 			useNewUrlParser: true,
 			useUnifiedTopology: true,
 		})
