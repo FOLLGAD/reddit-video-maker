af1f63cba8a012afe71dcbfe4db3cb75c117602c
Emil Ahlbäck <emil@tentium.se>
2021-01-21 15:42:14 +0100

    run cronjob

--- bog-api/src/server/cronjob.js
+++ bog-api/src/server/cronjob.js
@@ -3,7 +3,7 @@ const cron = require('cron')
 
 function deleteOldVideos() {
 	Video.deleteMany({ preview: true, created: { $lt: Date.now() - 1000 * 60 * 60 } }).exec() // delete if older than one hour
-	Video.deleteMany({ preview: false, created: { $lt: Date.now() - 1000 * 60 * 60 * 24 } }).exec() // delete if older than a day
+	Video.deleteMany({ preview: false, created: { $lt: Date.now() - 1000 * 60 * 60 * 24 * 7 } }).exec() // delete if older than 7 days
 }
 
 cron.job('0 0 4 * * *', deleteOldVideos) // Run at 4 am every day
