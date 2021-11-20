dc51286d8c35be962b9bd71cead335f7926ac899
Emil Ahlbäck <emil.ahlback99@gmail.com>
2021-11-20 23:53:09 +0100

    formatting

--- bog-api/src/utils/translation.js
+++ bog-api/src/utils/translation.js
@@ -1,26 +1,28 @@
-const fetch = require("node-fetch")
+const fetch = require("node-fetch");
 
 const translate = async (text, lang) => {
-    const details = {
-        text,
-        auth_key: process.env.DEEPL_KEY,
-        target_lang: lang,
-        tag_handling: "xml",
-        preserve_formatting: "1",
-    }
+  const details = {
+    text,
+    auth_key: process.env.DEEPL_KEY,
+    target_lang: lang,
+    tag_handling: "xml",
+    preserve_formatting: "1",
+  };
 
-    const res = await fetch("https://api-free.deepl.com/v2/translate", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8",
-            "User-Agent": "Reddit Video Maker",
-        },
-        body: new URLSearchParams(details).toString(),
-    }).then((r) => r.json())
+  const res = await fetch("https://api-free.deepl.com/v2/translate", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8",
+      "User-Agent": "Reddit Video Maker",
+    },
+    body: new URLSearchParams(details).toString(),
+  }).then((r) => r.json());
 
-    return res.translations
-}
+  return res.translations;
+};
 
 module.exports.translateText = function translateText(text, lang) {
-    return translate(text, lang).then((translations) => translations[0].text)
-}
+
+  return translate(text, lang).then((translations) => translations[0].text);
+
+};
