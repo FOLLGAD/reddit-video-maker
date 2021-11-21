3e4ac769b721564af9afd0c06d78115540eea8a5
Emil Ahlbäck <emil.ahlback99@gmail.com>
2021-11-21 10:59:26 +0100

    added helper rerender func

--- bog-api/src/server/utils.js
+++ bog-api/src/server/utils.js
@@ -1,195 +1,212 @@
-const { Song, File, Theme, Video } = require("./models")
+const { Song, File, Theme, Video } = require("./models");
 
-const { render } = require("../rendering/render")
-const db = require("./db")
+const { render } = require("../rendering/render");
+const db = require("./db");
 
-const jwt = require("jsonwebtoken")
-const fs = require("fs")
-const path = require("path")
-const uuid = require("uuid")
+const jwt = require("jsonwebtoken");
+const fs = require("fs");
+const path = require("path");
+const uuid = require("uuid");
 
-require("dotenv").config()
+require("dotenv").config();
 
-const tokenSecret = process.env.TOKEN_SECRET
+const tokenSecret = process.env.TOKEN_SECRET;
 
-const filesLocation = path.join(__dirname, "../../files")
-const toFilesDir = (file) => (file ? path.join(filesLocation, file) : null)
+const filesLocation = path.join(__dirname, "../../files");
+const toFilesDir = (file) => (file ? path.join(filesLocation, file) : null);
 const uuidFileName = (extension) =>
-    extension ? uuid() + "." + extension : uuid()
+  extension ? uuid() + "." + extension : uuid();
 
-const vidExtension = "mp4"
+const vidExtension = "mp4";
 
 function createToken(email) {
-    return new Promise((res, rej) => {
-        jwt.sign({ email }, tokenSecret, { expiresIn: "30d" }, (err, token) => {
-            if (err) return rej(err)
-            return res(token)
-        })
-    })
+  return new Promise((res, rej) => {
+    jwt.sign({ email }, tokenSecret, { expiresIn: "30d" }, (err, token) => {
+      if (err) return rej(err);
+      return res(token);
+    });
+  });
 }
 function verifyToken(token) {
-    return new Promise((res, rej) => {
-        jwt.verify(token, tokenSecret, (err, payload) => {
-            if (err) return rej(err)
-            return res(payload)
-        })
-    })
+  return new Promise((res, rej) => {
+    jwt.verify(token, tokenSecret, (err, payload) => {
+      if (err) return rej(err);
+      return res(payload);
+    });
+  });
 }
 
 function deleteFileCond(filename) {
-    // Delete the file
-    return new Promise((res, rej) => {
-        if (filename) {
-            fs.unlink(path.join(filesLocation, filename), (err) => {
-                if (err) rej(err)
-                else res()
-            })
-        }
-    })
+  // Delete the file
+  return new Promise((res, rej) => {
+    if (filename) {
+      fs.unlink(path.join(filesLocation, filename), (err) => {
+        if (err) rej(err);
+        else res();
+      });
+    }
+  });
 }
 
 async function syncRenderFromRequest(video, options, owner, callback) {
-    try {
-        const ret = await renderFromRequest(video, options, owner)
-        callback(null, ret)
-    } catch (err) {
-        callback(err, null)
-    }
+  try {
+    const ret = await renderFromRequest(video, options, owner);
+    callback(null, ret);
+  } catch (err) {
+    callback(err, null);
+  }
+}
+
+async function rerenderVideo(id) {
+  const vid = await Video.findOne({
+    _id: id,
+  }).select({
+    request_body: 1,
+  });
+
+  return renderFromRequest(vid._id, vid.request_body, vid.owner);
 }
 
 async function renderFromRequest(
-    video,
-    { options, questionData, commentData, name = null, preview = false },
-    owner
+  video,
+  { options, questionData, commentData, name = null, preview = false },
+  owner
 ) {
-    await db.connect()
+  await db.connect();
 
-    let theme
-    try {
-        theme = await Theme.findById(options.theme)
-            .populate("intro")
-            .populate("transition")
-            .populate("outro")
-        if (!theme) {
-            throw { error: "WRONG_THEME", status: 400 }
-        }
-    } catch (e) {
-        console.error(e)
-        throw { error: "NO_THEME", status: 400 }
+  let theme;
+  try {
+    theme = await Theme.findById(options.theme)
+      .populate("intro")
+      .populate("transition")
+      .populate("outro");
+    if (!theme) {
+      throw { error: "WRONG_THEME", status: 400 };
     }
+  } catch (e) {
+    console.error(e);
+    throw { error: "NO_THEME", status: 400 };
+  }
 
-    let song = options.song
-        ? await Song.findById(options.song, { file: 1 }).populate("file")
-        : null
+  let song = options.song
+    ? await Song.findById(options.song, { file: 1 }).populate("file")
+    : null;
 
-    let videoFile = await File.create({
-        filename: uuidFileName(vidExtension),
-    })
+  let videoFile = await File.create({
+    filename: uuidFileName(vidExtension),
+  });
 
-    await Video.updateOne(
-        { _id: video },
-        {
-            theme: theme._id,
-            name:
-                (name || questionData.title) +
-                (options.translate ? ` (${options.translate})` : ""),
-            file: videoFile._id,
-            owner,
-            preview,
-            failed: false,
-        }
-    )
-
-    try {
-        let renderOptions = Object.assign({}, theme, {
-            outPath: toFilesDir(videoFile.filename),
-            intro: toFilesDir(theme.intro && theme.intro.filename),
-            transition: toFilesDir(
-                theme.transition && theme.transition.filename
-            ),
-            outro: toFilesDir(theme.outro && theme.outro.filename),
-            song: song && toFilesDir(song.file.filename),
-            name: theme.name,
-            voice: theme.voice,
-            voiceSpeed: theme.voiceSpeed,
-            volume: theme.volume,
-            callToAction: theme.callToAction,
-            translate: options.translate,
-        })
-
-        await render(questionData, commentData, renderOptions)
-
-        return { vid: video }
-    } catch (err) {
-        console.error("WHole video failed:", err)
-        throw { vid: video }
+  await Video.updateOne(
+    { _id: video },
+    {
+      theme: theme._id,
+      name:
+        (name || questionData.title) +
+        (options.translate ? ` (${options.translate})` : ""),
+      file: videoFile._id,
+      owner,
+      preview,
+      failed: false,
     }
+  );
+
+  try {
+    let renderOptions = Object.assign({}, theme, {
+      outPath: toFilesDir(videoFile.filename),
+      intro: toFilesDir(theme.intro && theme.intro.filename),
+      transition: toFilesDir(theme.transition && theme.transition.filename),
+      outro: toFilesDir(theme.outro && theme.outro.filename),
+      song: song && toFilesDir(song.file.filename),
+      name: theme.name,
+      voice: theme.voice,
+      voiceSpeed: theme.voiceSpeed,
+      volume: theme.volume,
+      callToAction: theme.callToAction,
+      translate: options.translate,
+    });
+
+    await render(questionData, commentData, renderOptions);
+
+    return { vid: video };
+  } catch (err) {
+    console.error("WHole video failed:", err);
+    throw { vid: video };
+  }
 }
 
 async function legacyRenderFromRequest(
-    { options, questionData, commentData, name = null, preview = false },
-    owner
+  { options, questionData, commentData, name = null, preview = false },
+  owner
 ) {
-    let theme
-    try {
-        theme = await Theme.findById(options.theme)
-            .populate("intro")
-            .populate("transition")
-            .populate("outro")
-        if (!theme) {
-            throw { error: "WRONG_THEME", status: 400 }
-        }
-    } catch (e) {
-        throw { error: "NO_THEME", status: 400 }
+  let theme;
+  try {
+    theme = await Theme.findById(options.theme)
+      .populate("intro")
+      .populate("transition")
+      .populate("outro");
+    if (!theme) {
+      throw { error: "WRONG_THEME", status: 400 };
     }
+  } catch (e) {
+    throw { error: "NO_THEME", status: 400 };
+  }
 
-    let song = options.song
-        ? await Song.findById(options.song, { file: 1 }).populate("file")
-        : null
+  let song = options.song
+    ? await Song.findById(options.song, { file: 1 }).populate("file")
+    : null;
 
-    let videoFile = await File.create({
-        filename: uuidFileName(vidExtension),
-    })
+  let videoFile = await File.create({
+    filename: uuidFileName(vidExtension),
+  });
 
-    let vid = new Video({
-        theme: theme._id,
-        name: name || questionData.title,
-        file: videoFile._id,
-        owner,
-        preview,
-    })
-    await vid.save()
+  let vid = new Video({
+    theme: theme._id,
+    name: name || questionData.title,
+    file: videoFile._id,
+    owner,
+    preview,
+  });
+  await vid.save();
 
-    let renderOptions = Object.assign({}, theme, {
-        outPath: toFilesDir(videoFile.filename),
-        intro: toFilesDir(theme.intro && theme.intro.filename),
-        transition: toFilesDir(theme.transition && theme.transition.filename),
-        outro: toFilesDir(theme.outro && theme.outro.filename),
-        song: song && toFilesDir(song.file.filename),
-        name: theme.name,
-        voice: theme.voice,
-        voiceSpeed: theme.voiceSpeed,
-        volume: theme.volume,
-        callToAction: theme.callToAction,
-    })
+  let renderOptions = Object.assign({}, theme, {
+    outPath: toFilesDir(videoFile.filename),
+    intro: toFilesDir(theme.intro && theme.intro.filename),
+    transition: toFilesDir(theme.transition && theme.transition.filename),
+    outro: toFilesDir(theme.outro && theme.outro.filename),
+    song: song && toFilesDir(song.file.filename),
+    name: theme.name,
+    voice: theme.voice,
+    voiceSpeed: theme.voiceSpeed,
+    volume: theme.volume,
+    callToAction: theme.callToAction,
+  });
 
-    let renderPromise = render(questionData, commentData, renderOptions)
+  let renderPromise = render(questionData, commentData, renderOptions);
 
-    return {
-        renderPromise,
-        vid,
-    }
+  return {
+    renderPromise,
+    vid,
+  };
 }
 
 module.exports = {
-    vidExtension,
-    uuidFileName,
-    filesLocation,
-    toFilesDir,
-    createToken,
-    verifyToken,
-    deleteFileCond,
-    renderFromRequest,
-    syncRenderFromRequest,
-    legacyRenderFromRequest,
-}
+  vidExtension,
+
+  uuidFileName,
+
+  filesLocation,
+
+  toFilesDir,
+
+  createToken,
+
+  verifyToken,
+
+  deleteFileCond,
+
+  renderFromRequest,
+
+  syncRenderFromRequest,
+
+  legacyRenderFromRequest,
+};
