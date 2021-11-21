ac99a9ea0e153e6027ed0895a91ede575d2f1da6
Emil Ahlbäck <emil.ahlback99@gmail.com>
2021-11-21 11:02:50 +0100

    added pro/free deepl api url

--- bog-api/src/utils/translation.js
+++ bog-api/src/utils/translation.js
@@ -1,5 +1,10 @@
 const fetch = require("node-fetch");
 
+const IS_DEEPL_PRO = true;
+const apiUrl = IS_DEEPL_PRO
+  ? "https://api.deepl.com/v2/translate"
+  : "https://api-free.deepl.com/v2/translate";
+
 const translate = async (text, lang) => {
   const details = {
     text,
@@ -9,7 +14,7 @@ const translate = async (text, lang) => {
     preserve_formatting: "1",
   };
 
-  const res = await fetch("https://api-free.deepl.com/v2/translate", {
+  const res = await fetch(apiUrl, {
     method: "POST",
     headers: {
       "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8",
@@ -22,7 +27,5 @@ const translate = async (text, lang) => {
 };
 
 module.exports.translateText = function translateText(text, lang) {
-
   return translate(text, lang).then((translations) => translations[0].text);
-
 };
